cc038cb1f38674d6e32c78483c954517d88db543
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2022-07-06 13:32:15 +0200

    update workspace config

--- workspace/workspace-setup-local.sh
+++ workspace/workspace-setup-local.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-rsync -La ~/.zshrc ~/.p10k.zsh ~/.gitlabrc ~/.oh-my-zsh-custom ~/dev/dotfiles/workspace/.gitconfig -r workspace-francisco-puig:~
+rsync -Lar \
+  ~/.zshrc ~/.p10k.zsh ~/.gitlabrc ~/.oh-my-zsh-custom \
+  ~/dev/dotfiles/workspace/.gitconfig \
+  ~/.vimrc ~/.vim \
+  workspace-francisco-puig:~
+
+rsync -Lar \
+    ~/.config/nvim \
+  workspace-francisco-puig:~/.config/
+
+rsync -Lar \
+    ~/dev/dotfiles/workspace/pyrightconfig.json \
+    workspace-francisco-puig:~/dd/dogweb/
